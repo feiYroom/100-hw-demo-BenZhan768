ff61677968c40953c3a590e26f9b660b8daf8552
White Cat <117523297+White2Cat@users.noreply.github.com>
2025-07-20 23:55:25 +0800

    Update main.cpp

--- exercise00/main.cpp
+++ exercise00/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(void) {
     // TODO: Print the correct output for this exercise
-    cout << "Hello world!" << endl;
+    cout << "Hello, world!";
     
     return 0;
 }
